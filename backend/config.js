fa214ac40df012b29d0148498fc847cc72612832
lisaMekhael <lilia2004samir@gmail.com>
2024-05-14 15:24:38 +0300

    commit

--- backend/config.js
+++ backend/config.js
@@ -1,6 +1,23 @@
-import "dotenv/config";
+// import "dotenv/config";
 
-export const PORT = 5555;
+// export const PORT = 5555;
 
 
-export const mongoDBURL = "mongodb://localhost:27017/bookstore";
+// export const mongoDBURL = "mongodb://localhost:27017/bookstore";
+
+const config = {
+    database: {
+        host: 'localhost',
+        port: 27017,
+        dbName: 'myDatabase',
+        // Other database configuration options
+    },
+    apiKeys: {
+        apiKey1: 'your-api-key-1',
+        apiKey2: 'your-api-key-2',
+        // Other API keys
+    },
+    // Other configuration options
+};
+
+export default config;
--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,7 @@
 import express, { request, response } from "express";
-import { PORT, mongoDBURL } from "./config.js";
-import mongoose from "mongoose";
-import { Book } from "./models/bookModel.js";
+// import { PORT, mongoDBURL } from "./config.js";
+// import mongoose from "mongoose";
+// import { Book } from "./models/bookModel.js";
 import booksRoute from "./routes/booksRoute.js";
 import cors from "cors";
 
@@ -27,14 +27,49 @@ app.get("/", (request, response) => {
 
 app.use("/books", booksRoute);
 
-mongoose
-  .connect(mongoDBURL)
-  .then(() => {
-    console.log("App connected to db");
-    app.listen(PORT, () => {
-      console.log(`App listening on port ${PORT}!`);
-    });
-  })
-  .catch((error) => {
-    console.log(error);
-  });
+// mongoose
+//   .connect(mongoDBURL)
+//   .then(() => {
+//     console.log("App connected to db");
+//     app.listen(PORT, () => {
+//       console.log(`App listening on port ${PORT}!`);
+//     });
+//   })
+//   .catch((error) => {
+//     console.log(error);
+//   });
+
+
+import AWS from 'aws-sdk';
+import aks from './config.js';
+
+
+// Set the region and access keys
+AWS.config.update({
+region: aks.region,
+accessKeyId: aks.accessKeyId,
+secretAccessKey: aks.secretAccessKey
+});
+
+const docClient = new AWS.DynamoDB.DocumentClient();
+
+docClient.put({
+  TableName : 'Books',
+  Item:{
+    id:'101',
+    title:"The Catcher",
+    description: "This is a classic novel",
+    authour : "Henry L. Nail",
+    publishYear: '1999',
+    //S3 URL
+    coverPhotoUrl : "https://cloudproject66.s3.eu-north-1.amazonaws.com/Catcher+Book.jpeg" 
+  }
+
+}, (err,data)=>{
+  if(!err){
+    console.log("Data inserted Successfully!" , data);
+}else{
+  console.log("Error inserting data into database" , err);
+}
+
+})
